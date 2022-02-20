3d45e12bd74afc050f44021594b871cba33a529f
valentarayre <valentintarayre@gmail.com>
2022-02-20 09:48:48 -0300

    Fix delete, update, create to ApiFake

--- src/components/CrudApi.js
+++ src/components/CrudApi.js
@@ -20,37 +20,70 @@ const CrudApi = () => {
       .get(url)
       .then((res) => {
         console.log();
-        if (!res.err && res.length !== undefined) {          
+        if (!res.err && res.length !== undefined) {
           setDb(res);
           setError(null);
         } else {
           setDb(null);
-          res.status = res.status === undefined ? 500: res.status
-          res.statusText = res.statusText === undefined ? "No esta disponible la base de datos": res.status
+          res.status = res.status === undefined ? 500 : res.status;
+          res.statusText =
+            res.statusText === undefined
+              ? "No esta disponible la base de datos"
+              : res.status;
           setError(res);
-          
         }
         setLoading(false);
       });
-    
   }, [url]);
 
   const createData = (data) => {
-    data.id = Date.now();
-    setDb([...db, data]);
+    // data.id = Date.now();
+    const options = {
+      body: data,
+      headers: { "content-type": "application/json" },
+    };
+    api.post(url, options).then((res) => {
+      if (!res.err) {
+        setDb([...db, res]);
+      } else {
+        setError(res);
+      }
+    });
   };
 
   const updateData = (data) => {
-    const newData = db.map((el) => (el.id === data.id ? data : el));
-    setDb(newData);
+    const endPoint = `${url}/${data.id}`;
+
+    const options = {
+      body: data,
+      headers: { "content-type": "application/json" },
+    };
+    api.put(endPoint, options).then((res) => {
+      if (!res.err) {
+        const newData = db.map((el) => (el.id === data.id ? data : el));
+        setDb(newData);
+      } else {
+        setError(res);
+      }
+    });
   };
 
   const deleteData = (id) => {
     const isDelete = window.confirm("Estas Seguro de elimnar este registro");
-
     if (!isDelete) return;
-    const newData = db.filter((el) => el.id !== id);
-    setDb(newData);
+    const endPoint = `${url}/${id}`;
+
+    const options = {
+      headers: { "content-type": "application/json" },
+    };
+    api.del(endPoint, options).then((res) => {
+      if (!res.err) {
+        const newData = db.filter((el) => el.id !== id);
+        setDb(newData);
+      } else {
+        setError(res);
+      }
+    });
     if (dataToEdit !== null) setDataToEdit(null);
   };
 
@@ -63,8 +96,8 @@ const CrudApi = () => {
           updateData={updateData}
           dataToEdit={dataToEdit}
           setDataToEdit={setDataToEdit}
-        />        
-        {loading && <Loader />}        
+        />
+        {loading && <Loader />}
         {error && (
           <Message
             msg={`Error ${error.status}: ${error.statusText}`}
